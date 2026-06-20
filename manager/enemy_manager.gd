666f4c169dc96380edd3b1538591a10fbb7cab3e
SergeHD <sergiorivera252@gmail.com>
2026-06-20 13:13:40 -0400

    new_something

--- manager/enemy_manager.gd
+++ manager/enemy_manager.gd
@@ -7,12 +7,13 @@ const ENEMY_SPAWN_TIME_GROWTH: float = -0.15
 
 # Toll manager for enemy
 # -----------Name-----------Type----#
-@export var enemy_scene: PackedScene
+@export var enemy_scene: PackedScene 
 @export var enemy_sapwn_root: Node
 @export var spawn_rect: ReferenceRect
 
 @onready var spawn_interval_timer: Timer = $SpawnIntervalTimer
 @onready var round_timer: Timer = $RoundTimer
+
 
 var round_count: int = 0
 
@@ -20,11 +21,15 @@ var round_count: int = 0
 func _ready() -> void:
 	spawn_interval_timer.timeout.connect(_on_spawn_interval_timer_timeout)
 	round_timer.timeout.connect(_on_round_timer_timeout)
-	
+	begin_round()
 	
 func begin_round():
 	round_count += 1
-	round_timer.wait_time = ROUND_BASE_TIME + ((round_count - 1) * ROUND_GROWTH)
+	round_timer.wait_time = ROUND_BASE_TIME + ((round_count - 1) * ROUND_GROWTH) #The - 1 is just shifting so the growth starts /counting from zero instead of one.
+	round_timer.start()
+	
+	spawn_interval_timer.wait_time = BASE_ENEMY_SPAWN_TIME + ((round_count -1) * ENEMY_SPAWN_TIME_GROWTH)
+	spawn_interval_timer.start()
 	
 	
 func get_random_spawn_poaition() -> Vector2:
@@ -43,7 +48,10 @@ func spawn_enemy():
 func _on_spawn_interval_timer_timeout():
 	if is_multiplayer_authority():
 		spawn_enemy()
+		spawn_interval_timer.start()
 
 
 func _on_round_timer_timeout():
-	pass
+	if is_multiplayer_authority():
+		spawn_interval_timer.stop()
+		print("round over")
